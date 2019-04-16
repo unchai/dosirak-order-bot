f2f88de38ffcb852c96154a181dd7ab953845184
soohan.jang <soohan.jang@webtoonscorp.com>
2019-04-16 11:12:19 +0900

    메뉴 목록 변경
    - 설악산, 지리산, 한라산, 백두산 도시락 추가
    - 독도, 육해공, 김치찌개+밥, 육개장+밥 제거

--- src/menu-list.ts
+++ src/menu-list.ts
@@ -1,25 +1,32 @@
 import { IMenu } from './types';
 
 export default [
-    { name: '독도 도시락', price: 5500 },
-    { name: '양평우렁 강된장쌈밥', price: 7000 },
-    { name: '삼겹살 쌈밥도시락', price: 10000 },
-    { name: '우삼겹 쌈밥도시락', price: 8000 },
-    { name: '제육 쌈밥도시락', price: 7000 },
-    { name: '소불고기 쌈밥도시락', price: 7500 },
+    { name: '설악산 도시락', price: 6500},
+    { name: '지리산 도시락', price: 8000},
+    { name: '한라산 도시락', price: 10000},
+    { name: '백두산 도시락', price: 10000},
+
     { name: '서울 도시락', price: 5600 },
     { name: '대전 도시락', price: 5800 },
     { name: '대구 도시락', price: 5600 },
     { name: '부산 도시락', price: 5600 },
     { name: '울산 도시락', price: 5800 },
     { name: '광주 도시락', price: 5800 },
+
+    { name: '양평우렁 강된장쌈밥', price: 7000 },
+    { name: '삼겹살 쌈밥도시락', price: 10000 },
+    { name: '우삼겹 쌈밥도시락', price: 8000 },
+    { name: '제육 쌈밥도시락', price: 7000 },
+    { name: '소불고기 쌈밥도시락', price: 7500 },
+    
     { name: '하와이안 스테이크 도시락', price: 9000 },
     { name: '훌리훌리 도시락', price: 7500 },
-    { name: '육해공 도시락', price: 8000 },
     { name: '배꼽터진 왕돈까스 도시락', price: 6500 },
     { name: '목살 스테이크 도시락', price: 9000 },
     { name: '김치찌개세트', price: 5000 },
     { name: '육개장세트', price: 5000 },
-    { name: '김치찌개 + 밥', price: 4000 },
-    { name: '육개장 + 밥', price: 4000 },
+    
+    { name: '왕교자떡만둣국', price: 5500},
+    { name: '얼큰짬뽕우동', price: 6000},
+    { name: '얼큰짬뽕순두부', price: 6000},
 ] as IMenu[];
