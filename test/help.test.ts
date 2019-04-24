d17098f795fb177aea0cb3cd9e8c0dc7145d5cf8
Jaehyeon Nam <unchai@protonmail.com>
2019-04-24 11:03:38 +0900

    테스트코드 수정

--- test/help.test.ts
+++ test/help.test.ts
@@ -1,7 +1,17 @@
-import { findCommand } from './test-helper';
-
 describe('Tests', () => {
+    beforeEach(() => {
+        process.env = Object.assign(
+            process.env,
+            {
+                LINE_ACCESS_TOKEN: 'test_token',
+                LINE_SECRET: 'test_secret',
+            },
+        );
+    });
+
     it('Help command', async () => {
+        const { findCommand } = require('./test-helper');
+
         const command = findCommand('도움말');
         expect(command).not.toBeNull();
 
