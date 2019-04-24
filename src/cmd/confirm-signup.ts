97d22b15ad51c902d4aef39b6cd42fa706fdd50b
Jaehyeon Nam <unchai@protonmail.com>
2019-04-24 11:20:04 +0900

    가입승인 방법 수정

--- src/cmd/confirm-signup.ts
+++ src/cmd/confirm-signup.ts
@@ -11,27 +11,23 @@ export default {
         const users = await repo.getRequestSignUpUsers();
 
         const userNo = Number(args[1]);
-        const userIdHint = args[2];
-        const newName = args[3];
+        const newName = args[2];
 
-        if (userNo < 1 || userNo > users.length || !userIdHint) {
+        if (userNo < 1 || userNo > users.length) {
             throw new Error('가입 승인처리를 할 수 없습니다.');
         }
 
         const targetUser = users[userNo - 1];
-        const targetUserIdHint = targetUser.userId.substring(0, 4).toLowerCase();
-
-        if (userIdHint !== targetUserIdHint) {
-            throw new Error('해당 사용자의 ID가 틀립니다.');
-        }
-
-        targetUser.userName = newName ? newName : targetUser.userName;
         targetUser.confirm = true;
+
+        if (newName) {
+            targetUser.userName = newName;
+        }
 
         await repo.saveUser(targetUser);
 
         await lineClient.pushMessage(targetUser.userId, { type: 'text', text: '가입 요청이 승인되었습니다.' });
 
-        return `사용자 '${targetUser.userName}' (${targetUserIdHint})의 가입승인이 완료되었습니다.`;
+        return `사용자 '${targetUser.userName}'의 가입승인이 완료되었습니다.`;
     },
 } as ICommandFunc;
