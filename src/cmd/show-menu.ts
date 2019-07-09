c2c155d009f2aa1618b5e5e562cf63d37c7197da
soohan.jang <soohan.jang@webtoonscorp.com>
2019-07-09 13:54:05 +0900

    메뉴 명령어 링크 수정
    - "메뉴" 명령어 실행 시, 출력되는 문구 수정

--- src/cmd/show-menu.ts
+++ src/cmd/show-menu.ts
@@ -4,6 +4,6 @@ import { ICommandFunc } from '../types';
 export default {
     async execute(userId: string, args: string[]): Promise<string> {
         const menu = MENU_LIST.map((v, i) => `${i + 1}. ${v.name} (${v.price})`).join('\n');
-        return `오봉도시락 메뉴입니다.\nhttp://www.iobong.com/menu/list.php?mtype=B\n\n${menu}`;
+        return `토마토도시락 메뉴입니다.\nhttp://www.tomatodosirak.co.kr/board/index.php?board=menu_01&sca=new\n\n${menu}`;
     },
 } as ICommandFunc;
